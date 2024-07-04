025571b9b46d501f65f3d89acf3be0d7af7bc8ae
ketzon <bessonflorian42@yahoo.fr>
2024-07-04 21:31:22 +0200

    nvim laptop

---
+++ nvim/init.lua
@@ -0,0 +1,49 @@
+vim.g.mapleader = ' '
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("k.plugins")
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.swapfile = false
+vim.opt.tabstop = 4
+vim.opt.hidden = true
+vim.opt.autowrite = true
+
+-- Correcting the nnoremap statements
+vim.api.nvim_set_keymap('n', '<C-U>', '<C-U>zz', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-D>', '<C-D>zz', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-]>', '<C-]>zz', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-o>', '<C-o>zz', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-i>', '<C-i>zz', { noremap = true })
+
+-- nvim-tree binds 
+vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
+-- telescope binds
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
---
+++ nvim/lua/k/plugins/commentary.lua
@@ -0,0 +1,8 @@
+return {
+  'tpope/vim-commentary',
+  'tpope/vim-vinegar',
+  'tpope/vim-repeat',
+  'tpope/vim-unimpaired',
+  'romainl/vim-qf',
+  'ludovicchabant/vim-gutentags'
+}
