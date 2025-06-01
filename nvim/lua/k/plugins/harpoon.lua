cb43410932c9be9f4390855df0fc6ad218e8e4f0
florian <bessonflorian42@yahoo.fr>
2025-06-01 15:26:25 +0200

    update kitty et neorg

--- nvim/lua/k/plugins/harpoon.lua
+++ nvim/lua/k/plugins/harpoon.lua
@@ -2,6 +2,5 @@ return {
   "ThePrimeagen/harpoon",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    -- La configuration est déjà dans votre init.lua, pas besoin de la dupliquer ici
   end
 }
