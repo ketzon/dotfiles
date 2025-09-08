cbcade74ffc0446b74ec75673b4141ec71e6d395
florian <bessonflorian42@yahoo.fr>
2025-09-08 23:30:17 +0200

    ff

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -2,21 +2,15 @@ vim.g.mapleader = ' '
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+  vim.fn.system({ "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("k.plugins")
 
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
+vim.o.background = "dark"
+pcall(vim.cmd, "colorscheme gruvbox") 
 
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.number = true
@@ -27,48 +21,45 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.hidden = true
 vim.opt.hlsearch = false
-
-
 vim.o.autowrite = true
-vim.api.nvim_create_autocmd({"TextChanged", "TextChangedI"}, {
+
+vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
   pattern = "*",
   command = "silent! write"
 })
-		
 
--- Correcting the nnoremap statements
 vim.api.nvim_set_keymap('n', '<C-U>', '<C-U>zz', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-D>', '<C-D>zz', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-]>', '<C-]>zz', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-o>', '<C-o>zz', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-i>', '<C-i>zz', { noremap = true })
 
--- nvim-tree binds 
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
 
--- Harpoon
-local mark = require('harpoon.mark')
-local ui = require('harpoon.ui')
-vim.keymap.set('n', '<leader>m', mark.add_file)
-vim.keymap.set('n', '<leader>h', ui.toggle_quick_menu)
-vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end)
-vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end)
-vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end)
-vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end)
+local ok_mark, mark = pcall(require, 'harpoon.mark')
+local ok_ui, ui = pcall(require, 'harpoon.ui')
+if ok_mark and ok_ui then
+  vim.keymap.set('n', '<leader>m', mark.add_file)
+  vim.keymap.set('n', '<leader>h', ui.toggle_quick_menu)
+  vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end)
+  vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end)
+  vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end)
+  vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end)
+end
 
--- telescope binds
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fs', '<cmd>Telescope lsp_document_symbols<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>fS', '<cmd>Telescope lsp_workspace_symbols<CR>', { noremap = true, silent = true })
+local ok_telescope, builtin = pcall(require, 'telescope.builtin')
+if ok_telescope then
+  vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+  vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+  vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+  vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+  vim.keymap.set('n', '<leader>fs', '<cmd>Telescope lsp_document_symbols<CR>', { noremap = true, silent = true })
+  vim.keymap.set('n', '<leader>fS', '<cmd>Telescope lsp_workspace_symbols<CR>', { noremap = true, silent = true })
+end
 
--- autoindent de mort
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "html",
   callback = function()
@@ -77,4 +68,3 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.indentexpr = ""
   end
 })
-
