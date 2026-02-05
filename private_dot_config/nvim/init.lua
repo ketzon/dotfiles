ed92c5531dee92856145f7be61be35c89deaa856
florian <bessonflorian42@yahoo.fr>
2026-02-06 00:09:31 +0100

    change preview telescope

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -197,16 +197,44 @@ local actions = require('telescope.actions')
 
 telescope.setup({
   defaults = {
+    preview = { treesitter = true },
+    color_devicons = true,
+    sorting_strategy = "ascending",
+    borderchars = {
+      "", -- top
+      "", -- right
+      "", -- bottom
+      "", -- left
+      "", -- top-left
+      "", -- top-right
+      "", -- bottom-right
+      "", -- bottom-left
+    },
+    path_display = { "smart" },
+    layout_config = {
+      height = 100,
+      width = 400,
+      prompt_position = "top",
+      preview_cutoff = 40,
+    },
     mappings = {
       i = {
+        -- Navigation dans la liste
         ['<C-j>'] = actions.move_selection_next,
         ['<C-k>'] = actions.move_selection_previous,
         ['<C-n>'] = actions.move_selection_next,
         ['<C-e>'] = actions.move_selection_previous,
+        -- Scroll vertical dans la preview
+        ['<C-u>'] = actions.preview_scrolling_up,
+        ['<C-d>'] = actions.preview_scrolling_down,
       },
       n = {
+        -- Navigation dans la liste
         ['<C-j>'] = actions.move_selection_next,
         ['<C-k>'] = actions.move_selection_previous,
+        -- Scroll vertical dans la preview
+        ['<C-u>'] = actions.preview_scrolling_up,
+        ['<C-d>'] = actions.preview_scrolling_down,
       },
     },
   },
